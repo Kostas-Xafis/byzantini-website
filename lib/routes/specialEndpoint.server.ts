6e867fb62ad606df101268dea9c8c3f1ed8b0aec
Κωνσταντινος Χαφης <koxafis@gmail.com>
2023-10-16 14:07:26 +0300

    Removed jsonld stuff for now & Fixed administration images

--- lib/routes/specialEndpoint.server.ts
+++ lib/routes/specialEndpoint.server.ts
@@ -6,10 +6,10 @@ import type { APIContext } from "astro";
 // Include this in all .server.ts files
 const serverRoutes = JSON.parse(JSON.stringify(SpecialRoutes)) as typeof SpecialRoutes; // Copy the routes object to split it into client and server routes
 
-const imageMIMEType = ["image/jpeg", "image/png", "image/webp", "image/gif", "image/jfif", "image/jpg"];
+// const imageMIMEType = ["image/jpeg", "image/png", "image/webp", "image/gif", "image/jfif", "image/jpg"];
 
-const cvPrefix = "kathigites/cv/";
-const picturePrefix = "kathigites/picture/";
+// const cvPrefix = "kathigites/cv/";
+// const picturePrefix = "kathigites/picture/";
 // const moveFilesToDirectories = (ctx: APIContext) => execTryCatch(async () => {
 // 	const teachers = await executeQuery<{ fullname: string, cv?: string; picture?: string; }>("SELECT fullname, picture, cv FROM teachers");
 // 	let res = "";
@@ -40,22 +40,22 @@ const picturePrefix = "kathigites/picture/";
 // 	return res;
 // });
 
-const deleteOldFiles = (ctx: APIContext) => execTryCatch(async () => {
-	const teachers = await executeQuery<{ fullname: string, cv?: string; picture?: string; }>("SELECT picture, cv FROM teachers");
-	for (const { cv, picture } of teachers) {
-		if (cv) await Bucket.delete(ctx, cv);
-		if (picture) await Bucket.delete(ctx, picture);
-	}
-	return "Successfully deleted old files";
-});
-
+// const deleteOldFiles = (ctx: APIContext) => execTryCatch(async () => {
+// 	const teachers = await executeQuery<{ fullname: string, cv?: string; picture?: string; }>("SELECT picture, cv FROM teachers");
+// 	for (const { cv, picture } of teachers) {
+// 		if (cv) await Bucket.delete(ctx, cv);
+// 		if (picture) await Bucket.delete(ctx, picture);
+// 	}
+// 	return "Successfully deleted old files";
+// });
+//
 serverRoutes.execute.func = async (ctx, slug) => {
 	const { func } = slug;
 	switch (func) {
 		// case "moveFilesToDirectories":
 		// 	return await moveFilesToDirectories(ctx);
-		case "deleteOldFiles":
-			return await deleteOldFiles(ctx);
+		// case "deleteOldFiles":
+		// 	return await deleteOldFiles(ctx);
 		default: return await execTryCatch(async () => "Invalid function");
 	}
 };
