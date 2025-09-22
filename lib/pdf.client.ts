ed5d67c722ac4467115f3db23558ac936101bf2b
Kostas <koxafis@gmail.com>
2025-09-22 14:31:16 +0300

    On pdf handling: better error handling and single file passing in bulk methods. Also moved some functions closer together.

--- lib/pdf.client.ts
+++ lib/pdf.client.ts
@@ -35,7 +35,66 @@ export class PDF {
 		return `${this.teachersName}/${this.student.first_name}_${this.student.last_name}.pdf`;
 	}
 
+	private static loadPrintJS() {
+		return loadScript("https://cdnjs.cloudflare.com/ajax/libs/print-js/1.6.0/print.min.js", () => !!window["printJS"]);
+	};
+
+	public async print() {
+		await PDF.loadPrintJS();
+		const body: PDFRequest = PDFTypeWrap("registration", {
+			isMultiple: false,
+			data: {
+				url: PDF.TemplateFileName[this.student.class_id],
+				student: this.student,
+				teachersName: this.teachersName,
+				instrument: this.instrument,
+			}
+		});
+		const imgBlob = await (await fetch(PDF.PDFWorkerURL, {
+			method: "POST",
+			headers: {
+				"Authorization": `Bearer ${getCookie("session_id")}`
+			},
+			body: JSON.stringify(body)
+		})).blob();
+		const fileURL = URL.createObjectURL(imgBlob);
+		const printWindow = window.open(fileURL, "_blank");
+		printWindow?.print();
+	}
+
+	public static async printBulk(pdfs: PDF[]) {
+		if (pdfs.length === 0) throw new Error("No PDFs to print");
+
+		await PDF.loadPrintJS();
+
+		if (pdfs.length === 1) {
+			return pdfs[0].print();
+		}
+
+		const body: PDFRequest = PDFTypeWrap("registration", {
+			isMultiple: true,
+			data: pdfs.map((pdf) => ({
+				url: PDF.TemplateFileName[pdf.student.class_id],
+				student: pdf.student,
+				teachersName: pdf.teachersName,
+				instrument: pdf.instrument,
+			}))
+		});
+		const imgBlob = await (await fetch(PDF.PDFWorkerURL, {
+			method: "POST",
+			headers: {
+				"Authorization": `Bearer ${getCookie("session_id")}`
+			},
+			body: JSON.stringify(body)
+		})).blob();
+		const fileURL = URL.createObjectURL(imgBlob);
+		const printWindow = window.open(fileURL, "_blank");
+		printWindow?.print();
+	}
+
 	public async download(): Promise<void> {
+		if (!this.student.first_name || !this.student.last_name) throw new Error("Student data not set");
+
 		const body: PDFRequest = PDFTypeWrap("registration", {
 			isMultiple: false,
 			data: {
@@ -59,82 +118,13 @@ export class PDF {
 		a.click();
 	}
 
-	public async print() {
-		await loadScript("https://cdnjs.cloudflare.com/ajax/libs/print-js/1.6.0/print.min.js", () => !!window["printJS"]);
-		const body: PDFRequest = PDFTypeWrap("registration", {
-			isMultiple: false,
-			data: {
-				url: PDF.TemplateFileName[this.student.class_id],
-				student: this.student,
-				teachersName: this.teachersName,
-				instrument: this.instrument,
-			}
-		});
-		const imgBlob = await (await fetch(PDF.PDFWorkerURL, {
-			method: "POST",
-			headers: {
-				"Authorization": `Bearer ${getCookie("session_id")}`
-			},
-			body: JSON.stringify(body)
-		})).blob();
-		const fileURL = URL.createObjectURL(imgBlob);
-		const printWindow = window.open(fileURL, "_blank");
-		printWindow?.print();
-	}
-
-	public static async convertFirstPageToImage(file: ArrayBuffer | string): Promise<string> {
-		const { getDocument, GlobalWorkerOptions } = await dynamicImport<typeof import("pdfjs-dist")>("https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.8.69/pdf.min.mjs", "pdfjsLib");
-		if (!GlobalWorkerOptions.workerSrc)
-			GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.8.69/pdf.worker.min.mjs';
-
-		// Load the PDF document
-		const pdf = await getDocument({ data: file }).promise;
-
-		// Get the first page
-		const page = await pdf.getPage(1);
-
-		// Set up a canvas with page dimensions
-		const viewport = page.getViewport({ scale: 1 });
-		const canvas = document.createElement('canvas');
-		const context = canvas.getContext('2d')!;
-		canvas.width = viewport.width;
-		canvas.height = viewport.height;
-
-		// Render the page onto the canvas
-		await page.render({
-			canvasContext: context,
-			viewport: viewport,
-		}).promise;
-
-		// Convert the canvas to a data URL
-		return canvas.toDataURL('image/png');
-	}
-
-	public static async printBulk(pdfs: PDF[]) {
-		await loadScript("https://cdnjs.cloudflare.com/ajax/libs/print-js/1.6.0/print.min.js", () => !!window["printJS"]);
-
-		const body: PDFRequest = PDFTypeWrap("registration", {
-			isMultiple: true,
-			data: pdfs.map((pdf) => ({
-				url: PDF.TemplateFileName[pdf.student.class_id],
-				student: pdf.student,
-				teachersName: pdf.teachersName,
-				instrument: pdf.instrument,
-			}))
-		});
-		const imgBlob = await (await fetch(PDF.PDFWorkerURL, {
-			method: "POST",
-			headers: {
-				"Authorization": `Bearer ${getCookie("session_id")}`
-			},
-			body: JSON.stringify(body)
-		})).blob();
-		const fileURL = URL.createObjectURL(imgBlob);
-		const printWindow = window.open(fileURL, "_blank");
-		printWindow?.print();
-	}
-
 	public static async downloadBulk(arr: PDF[], progressCallback?: (prog: number) => any): Promise<void> {
+		if (arr.length === 0) throw new Error("No PDFs to download");
+		if (arr.length === 1) {
+			return arr[0].download();
+		}
+
+		// Use queue to limit number of concurrent requests
 		const namesArr: string[] = [];
 		const requestArr = arr.map((pdf) => async () => {
 			let expoTime = 1000;
@@ -192,6 +182,35 @@ export class PDF {
 		a.click();
 		a.remove();
 	}
+
+	public static async convertFirstPageToImage(file: ArrayBuffer | string): Promise<string> {
+		const { getDocument, GlobalWorkerOptions } = await dynamicImport<typeof import("pdfjs-dist")>("https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.8.69/pdf.min.mjs", "pdfjsLib");
+		if (!GlobalWorkerOptions.workerSrc)
+			GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.8.69/pdf.worker.min.mjs';
+
+		// Load the PDF document
+		const pdf = await getDocument({ data: file }).promise;
+
+		// Get the first page
+		const page = await pdf.getPage(1);
+
+		// Set up a canvas with page dimensions
+		const viewport = page.getViewport({ scale: 1 });
+		const canvas = document.createElement('canvas');
+		const context = canvas.getContext('2d')!;
+		canvas.width = viewport.width;
+		canvas.height = viewport.height;
+
+		// Render the page onto the canvas
+		await page.render({
+			canvasContext: context,
+			viewport: viewport,
+		}).promise;
+
+		// Convert the canvas to a data URL
+		return canvas.toDataURL('image/png');
+	}
+
 }
 
 export const loadXLSX = async () => {
