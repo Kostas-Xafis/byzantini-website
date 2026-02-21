079d41e5d6f29c39a06676a2f8c80deaef301cf1
Kostas <koxafis@gmail.com>
2026-02-21 20:13:22 +0200

    Forgotten fix

--- lib/utilities/Google.ts
+++ lib/utilities/Google.ts
@@ -1,13 +1,16 @@
 import { Env } from "@env/env";
 import { Google } from "arctic";
+import { getOriginFromContext } from "./url";
+import type { APIContext } from "astro";
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = Env.env;
-if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
-    throw new Error("Google Client ID or Secret not set in environment variables");
-}
-
-export const google = new Google(
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    "http://localhost:4321/oauth2callback" // Change this to your actual redirect URI
-);
+export function google(ctx?: APIContext) {
+    const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = Env.env;
+    if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
+        throw new Error("Google Client ID or Secret not set in environment variables");
+    }
+    return new Google(
+        GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET,
+        `${getOriginFromContext(ctx)}/oauth2callback`
+    );
+};
